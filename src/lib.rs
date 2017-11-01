624bdbf02d9f517edfe9778d9498fb59a695aeba
Ken Reed <kenakioreed@gmail.com>
2017-10-31 21:49:32 -0400

    j

--- src/lib.rs
+++ src/lib.rs
@@ -151,8 +151,8 @@ impl<T> GenTree<T> {
             match a.into_get_mut_and_next(){
                 (xx,Some((left,right)))=>{
 
-                    rec(left,func);    
-                    func(xx);                
+                    func(xx);
+                    rec(left,func);                    
                     rec(right,func);
                 },
                 (xx,None)=>{
@@ -175,9 +175,9 @@ impl<T> GenTree<T> {
             match a.into_get_mut_and_next(){
                 (xx,Some((left,right)))=>{
                     rec(right,func);
-                    func(xx);
-                    rec(left,func);
                     
+                    rec(left,func);
+                    func(xx);
                     
 
                 },
@@ -236,9 +236,9 @@ impl<T> GenTree<T> {
                     
                     let aaa=dx.next();
 
-                    rec(left,func,aaa.clone());  
+
                     func(a.into_inner(),dx.get());
-                                      
+                    rec(left,func,aaa.clone());                    
                     rec(right,func,aaa);
                 },
                 None=>{
@@ -261,7 +261,6 @@ impl<T> GenTree<T> {
             match a.get_mut_and_next(){
                 (nn,Some((left,right)))=>{
                     
-                    rec(left,func);
                     {
                         let node=unsafe{
                             let mut node=std::mem::uninitialized::<T>();
@@ -273,6 +272,7 @@ impl<T> GenTree<T> {
 
                     }
                     
+                    rec(left,func);
                     rec(right,func);
                 },
                 (nn,None)=>{
